95bda50a3316b03d4702069fdee23045886abf6b
whiteCcinn <471113744@qq.com>
2019-07-23 11:24:28 +0800

    init

--- orange/sh/docker-entrypoint.sh
+++ orange/sh/docker-entrypoint.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+default_host='127.0.0.1'
+default_port=3306
+default_database='orange'
+default_root='root'
+default_password=''
+
 ORANGE_CONF=conf/orange.conf
+ORANGE_HOST=${MYSQL_HOST:-default_host}
+ORANGE_PORT=${MYSQL_PORT:-default_port}
+ORANGE_DATABASE=${MYSQL_DATABASE:-default_database}
+ORANGE_USER=${MYSQL_USER:-default_root}
+ORANGE_PWD=${MYSQL_PWD:-default_password}
 
 if [[ ! -e /www/manifest/orangelib.meta ]];then
   opm install sumory/lor
-  opm --install-dir=./ get zhangbao0325/orangelib=1.0.0
+  opm --install-dir=./ get zhangbao0325/orangelib
   luarocks install luafilesystem luasocket lrandom
   cp -v conf/{orange.conf.example,orange.conf}
   cp -v conf/{nginx.conf.example,nginx.conf}
@@ -14,4 +25,10 @@ if [[ ! -e /www/manifest/orangelib.meta ]];then
   sed -i "s/\"database\": \"orange\"/\"database\": \"${ORANGE_DATABASE}\"/g" ${ORANGE_CONF}
   sed -i "s/\"user\": \"root\"/\"user\": \"${ORANGE_USER}\"/g" ${ORANGE_CONF}
   sed -i "s/\"password\": \"\"/\"password\": \"${ORANGE_PWD}\"/g" ${ORANGE_CONF}
+fi
+
+cmd=$1
+
+if [[ "${cmd}" != 'bash' ]];then
+  bash /www/start.sh
 fi
